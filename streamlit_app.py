3d7df5eaab83eacb3885591c40a782051d07273d
ducatina <119556943+ducatina@users.noreply.github.com>
2023-04-05 22:38:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title ('Mr Montenegro')
+streamlit.title ('My parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg') 
+streamlit.text('🥑🍞Avocado Toast')
