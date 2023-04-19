84076bdb02bfc5f9eb1813a72f9a6b7b391c9ab9
ducatina <119556943+ducatina@users.noreply.github.com>
2023-04-19 15:46:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,7 +78,7 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add Fruit TO THE List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
-     my_cnx.close()
+    my_cnx.close()
     streamlit.text(back_from_function)      
 # my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
